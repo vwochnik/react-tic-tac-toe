ac3a62167ba2b48de9988dddc497a6a0d5215d9a
Vincent Wochnik <v.wochnik@gmail.com>
2018-08-22 18:46:26 +0200

    use loops

--- src/Board.js
+++ src/Board.js
@@ -3,33 +3,28 @@ import Square from './Square';
 
 export default class Board extends Component {
   renderSquare(i) {
-    return (
-      <Square
-        onClick={() => this.props.onClick(i)}
-        value={this.props.squares[i]}
-      />
-    );
   }
 
   render() {
+    const rows = [0, 1, 2].map((rowIndex) => {
+      const columns = [0, 1, 2].map((columnIndex) => {
+        const i = rowIndex * 3 + columnIndex;
+
+        return (
+          <Square
+            onClick={() => this.props.onClick(i)}
+            value={this.props.squares[i]}
+          />
+        );
+      });
+
+      return (
+        <div className="board-row">{columns}</div>
+      );
+    });
+
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div>{rows}</div>
     );
   }
 }
